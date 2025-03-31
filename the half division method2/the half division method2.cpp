5b45df20cb10af6d72852f659cc8e00ee0ab2d84
Бонаква <zefirax89@gmail.com>
2025-03-31 08:41:59 +0300

    Add files via upload

---
+++ the half division method2/the half division method2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+float f(float x) {
+	float y;
+	y = x * sin(x) - 1;
+	return y;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double a, b, c, exp, x;
+	cout << "Введите а=";
+	cin >> a;
+	cout << endl;
+	cout << "Введите b=";
+	cin >> b;
+	cout << endl;
+	cout << "Введите exp=";
+	cin >> exp;
+	cout << endl;
+	do {
+		c = (a + b) / 2;
+		if (f(a) * f(c) < 0) {
+			b = c;
+		}
+		else {
+			a = c;
+		}
+	} while (b - a > exp); {
+		x = (a + b) / 2;
+		cout.setf(ios::scientific);
+		cout << "X=" << x << endl;
+
+	}
+}
